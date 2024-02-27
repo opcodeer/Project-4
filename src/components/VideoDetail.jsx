18cb683f3690cd0ee5af4e8ac50c0c7c5dbf959c
gautam gautam <gautamdtua42003@gmail.com>
2024-02-27 21:09:18 +0530

    Refactoring some small details of website

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -37,21 +37,22 @@ const VideoDetail = () => {
         <Box flex={1}>
           <Box sx={{ width: "100%", position: "sticky", top: "15px" }}>
             <ReactPlayer url={`https://www.youtube.com/watch?v=${id}`} className="react-player" controls />
-            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
-              {title}
+            <Typography color="#fff" variant="body1" fontWeight="bold" fontSize='1.5rem' p={2}>
+                {title}
             </Typography>
             <Stack direction="row" justifyContent="space-between" sx={{ color: "#fff" }} py={1} px={2}>
-              <Link to={`/channel/${channelId}`}>
-                <Typography variant={{ sm: "subtitle1", md: "h6" }} color="#fff">
-                  {channelTitle}
-                  <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
-                </Typography>
+            <Typography variant="body1" color="#fff">
+              <Link to={`/channel/${channelId}`} style={{ color: '#fff', textDecoration: 'none' }}>
+                 {channelTitle}
+              <CheckCircleIcon sx={{ fontSize: "12px", color: "gray", ml: "5px" }} />
               </Link>
+             </Typography>
               <Stack direction="row" gap="20px" alignItems="center">
-                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                <Typography variant="body1" color="#fff" sx={{ opacity: 0.7 }}>
                   {parseInt(viewCount).toLocaleString()} views
                 </Typography>
-                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+
+                <Typography variant="body1" color="#fff" sx={{ opacity: 0.7 }}>
                   {parseInt(likeCount).toLocaleString()} likes
                 </Typography>
               </Stack>
